b1ec3db23b13215cf49996d998bc782e7fd14af6
Rahul M Hanchate <47830216+rmhanchate@users.noreply.github.com>
2019-12-05 00:28:42 +0530

    Update Third task.ino

--- Third task.ino
+++ Third task.ino
@@ -124,6 +124,137 @@ void MotorStop(void)
   digitalWrite(rightMotorBackward,LOW);
 }
 
+///////////////////////////////////////////////////////////////////////////////
+/*
+#include <Wire.h>
+#include <ESP8266WiFi.h>
+//#include <MPU6050.h>
+
+int16_t Acc_rawX, Acc_rawY, Acc_rawZ,Gyr_rawX, Gyr_rawY, Gyr_rawZ;
+float Acceleration_angle[2];
+float Gyro_angle[2];
+float Total_angle[2];
+float elapsedTime, time, timePrev;
+int i;
+float rad_to_deg = 180/3.141592654;
+float pid, pwmLeft, pwmRight, error, previous_error;
+float pid_p = 0;
+float pid_i = 0;
+float pid_d = 0;
+double kp = 3.55;//3.55
+double ki = 0.005;//0.003
+double kd = 2.05;//2.05
+double throttle=1300; //initial value of throttle to the motors
+//float desired_angle = 0; 
+
+WiFiClient client;
+WiFiServer server(80);
+const char* ssid = "hotspot";
+const char* password = "password";
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin(); //begin the wire comunication
+  Wire.beginTransmission(0x68);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+  Serial.begin(250000);
+  //right_prop.attach(3); //attatch the right motor to pin 3
+  //left_prop.attach(5);  //attatch the left motor to pin 5
+
+  server.begin();
+  
+  time = millis(); //Start counting time in milliseconds
+  delay(7000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  client = server.available();
+  if (!client) return; 
+  data = checkClient ();
+
+
+  
+  timePrev = time;  // the previous time is stored before the actual time read
+  time = millis();  // actual time read
+  elapsedTime = (time - timePrev) / 1000;
+   
+  Wire.beginTransmission(0x68);
+  Wire.write(0x3B); //Ask for the 0x3B register- correspond to AcX
+  Wire.endTransmission(false);
+  Wire.requestFrom(0x68,6,true);
+  Acc_rawX=Wire.read()<<8|Wire.read(); //each value needs two registres
+  Acc_rawY=Wire.read()<<8|Wire.read();
+  Acc_rawZ=Wire.read()<<8|Wire.read();
+  Acceleration_angle[0] = atan((Acc_rawY/16384.0)/sqrt(pow((Acc_rawX/16384.0),2) + pow((Acc_rawZ/16384.0),2)))*rad_to_deg;
+  Acceleration_angle[1] = atan(-1*(Acc_rawX/16384.0)/sqrt(pow((Acc_rawY/16384.0),2) + pow((Acc_rawZ/16384.0),2)))*rad_to_deg;
+  
+  Wire.beginTransmission(0x68);
+  Wire.write(0x43); //Gyro data first adress
+  Wire.endTransmission(false);
+  Wire.requestFrom(0x68,4,true); //Just 4 registers
+  Gyr_rawX=Wire.read()<<8|Wire.read(); //Once again we shif and sum
+  Gyr_rawY=Wire.read()<<8|Wire.read();
+  Gyro_angle[0] = Gyr_rawX/131.0;
+  Gyro_angle[1] = Gyr_rawY/131.0;
+  
+  Total_angle[0] = 0.98 *(Total_angle[0] + Gyro_angle[0]*elapsedTime) + 0.02*Acceleration_angle[0];
+  Total_angle[1] = 0.98 *(Total_angle[1] + Gyro_angle[1]*elapsedTime) + 0.02*Acceleration_angle[1];
+
+  error = Total_angle[1] - desired_angle;
+  pid_p = kp*error;
+  if(-3 <error <3)
+  {
+    pid_i = pid_i+(ki*error);  
+  }
+  pid_d = kd*((error - previous_error)/elapsedTime);
+  if (abs(error) >= 5.0){
+  pid = pid_p + pid_i + pid_d;
+  if(pid < -1000)
+  {
+    pid = -1000;
+  }
+  if(pid > 1000)
+  {
+    pid = 1000;
+  }
+  pwmLeft = throttle + PID;
+  pwmRight = throttle - PID;
+  moveBot(pwmLeft, pwmRight, 0);
+  }
+  else
+  {
+    pastErrors = 0;
+    pastError = 0;
+    Serial.println("All done!");
+    moveBot(0, 0, 0);
+  }
+}
+
+void moveBot(int left, int right, int del){
+  if (left < 0){
+    digitalWrite(in2, HIGH);
+  }
+  else{
+    digitalWrite(in2, LOW);
+  }
+  if (right < 0){
+    digitalWrite(in3, LOW);
+  }
+  else{
+    digitalWrite(in3, HIGH);
+  }
+  int sL = abs(left);
+  int sR = abs(right);
+  analogWrite(12, sL);
+  analogWrite(13, sR);    
+  if (del) delay(del);
+}
+*/
+//////////////////////////////////////////////////////////////////////////////
+
 //RECEIVE DATA FROM the second task (path finding)
 //Under progress
 //In python file:
